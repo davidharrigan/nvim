f8255ee625f62197d18bc32ddcd212ad7838444f
David Harrigan <davidharrigan@users.noreply.github.com>
2023-07-30 15:55:34 -0400

    chore: prevent auto loading session

--- lua/editor/session.lua
+++ lua/editor/session.lua
@@ -34,16 +34,16 @@ M.plugins = {
   },
 }
 
-M.autocmds = function()
-  vim.api.nvim_create_autocmd('VimEnter', {
-    group = vim.api.nvim_create_augroup('restore_session', { clear = true }),
-    callback = function()
-      if vim.fn.getcwd() ~= vim.env.HOME then
-        require('persistence').load()
-      end
-    end,
-    nested = true,
-  })
-end
+-- M.autocmds = function()
+--   vim.api.nvim_create_autocmd('VimEnter', {
+--     group = vim.api.nvim_create_augroup('restore_session', { clear = true }),
+--     callback = function()
+--       if vim.fn.getcwd() ~= vim.env.HOME then
+--         require('persistence').load()
+--       end
+--     end,
+--     nested = true,
+--   })
+-- end
 
 return M
