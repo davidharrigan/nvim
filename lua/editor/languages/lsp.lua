4c94b4150dffdb3f8b2b08ba899280a9c64d9818
David Harrigan <davidharrigan@users.noreply.github.com>
2023-07-30 10:27:26 -0400

    fix: session restore on open

--- lua/editor/languages/lsp.lua
+++ lua/editor/languages/lsp.lua
@@ -9,12 +9,12 @@ M.plugins = {
   {
     'neovim/nvim-lspconfig',
     tag = 'v0.1.6',
-    event = { "BufReadPre", "BufNewFile" },
+    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = {
       {
         'williamboman/mason.nvim',
         tag = 'v1.6.1',
-        build = ":MasonUpdate",
+        build = ':MasonUpdate',
       },
       {
         'williamboman/mason-lspconfig.nvim',
@@ -26,19 +26,19 @@ M.plugins = {
 }
 
 M.setup = function()
-  require("mason").setup()
+  require('mason').setup()
   require('neodev').setup()
 
-  require("mason-lspconfig").setup({ensure_installed=servers})
+  require('mason-lspconfig').setup({ ensure_installed = servers })
 
-  require("mason-lspconfig").setup_handlers {
+  require('mason-lspconfig').setup_handlers({
     -- The first entry (without a key) will be the default handler
     -- and will be called for each installed server that doesn't have
     -- a dedicated handler.
-    function (server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {}
+    function(server_name) -- default handler (optional)
+      require('lspconfig')[server_name].setup({})
     end,
-  }
+  })
 end
 
 return M
--- lua/editor/session.lua
+++ lua/editor/session.lua
@@ -11,14 +11,27 @@ M.plugins = {
 
 M.setup = function()
   require("persistence").setup ({
-    options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp" } ,
+    -- https://neovim.io/doc/user/options.html#'sessionoptions'
+    options = { "curdir", "winsize" },
   })
 end
 
 M.keymaps = function()
-    -- { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-    -- { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-    -- { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+  vim.keymap.set('n', "<leader>qs", function() require("persistence").load() end, {desc = "Restore Session" })
+  vim.keymap.set('n', "<leader>ql", function() require("persistence").load({ last = true }) end, {desc = "Restore Last Session" })
+  vim.keymap.set('n', "<leader>qd", function() require("persistence").stop() end, {desc = "Don't Save Current Session" })
+end
+
+M.autocmds = function()
+  vim.api.nvim_create_autocmd("VimEnter", {
+    group = vim.api.nvim_create_augroup("restore_session", { clear = true }),
+    callback = function()
+      if vim.fn.getcwd() ~= vim.env.HOME then
+        require("persistence").load()
+      end
+    end,
+    nested = true,
+  })
 end
 
 return M
