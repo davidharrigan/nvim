4c8a0475734515f876136015287f4fe4b0b2046c
David Harrigan <davidharrigan@users.noreply.github.com>
2024-03-27 12:21:14 -0400

    fix: tiltfile

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,17 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("mylazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  group = augroup("tiltfile"),
+  pattern = {
+    "Tiltfile*",
+    "*.tilt"
+  },
+  callback = function()
+    vim.opt_local.filetype = "tiltfile"
+  end,
+})
---
+++ lua/plugins/lang.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        tilt_ls = {},
+      },
+    },
+  },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, {
+        "go",
+        "gomod",
+        "starlark",
+      })
+      return opts
+    end,
+    config = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        ---@type table<string, boolean>
+        local added = {}
+        opts.ensure_installed = vim.tbl_filter(function(lang)
+          if added[lang] then
+            return false
+          end
+          added[lang] = true
+          return true
+        end, opts.ensure_installed)
+      end
+      require("nvim-treesitter.configs").setup(opts)
+
+      -- additional config
+      vim.treesitter.language.register("starlark", "tiltfile")
+    end,
+  },
+}
