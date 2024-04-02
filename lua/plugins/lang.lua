745548853eb06773e7086ea2e02321edf3db7278
David Harrigan <davidharrigan@users.noreply.github.com>
2024-04-02 16:38:23 -0400

    fix: update go lsp

--- lua/plugins/lang.lua
+++ lua/plugins/lang.lua
@@ -1,4 +1,7 @@
+local lspconfig = require("lspconfig")
+
 return {
+  -- Tiltfile
   {
     "neovim/nvim-lspconfig",
     ---@class PluginLspOpts
@@ -9,16 +12,11 @@ return {
       },
     },
   },
-
-  -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       opts.ensure_installed = vim.list_extend(opts.ensure_installed, {
-        "go",
-        "gomod",
         "starlark",
-        "golang",
       })
       return opts
     end,
@@ -39,5 +37,104 @@ return {
       -- additional config
       vim.treesitter.language.register("starlark", "tiltfile")
     end,
+  },
+
+  -- Go
+  -- Extends: { import = "lazyvim.plugins.extras.lang.go" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = vim.list_extend(opts.ensure_installed, {
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+      })
+      return opts
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "golangci-lint",
+        "golangci-lint-langserver",
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        golangci_lint_ls = {},
+        gopls = {
+          keys = {
+            -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+            { "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
+          },
+          settings = {
+            gopls = {
+              gofumpt = true,
+              codelenses = {
+                gc_details = false,
+                generate = true,
+                regenerate_cgo = true,
+                run_govulncheck = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              analyses = {
+                fieldalignment = false,
+                nilness = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              usePlaceholders = false,
+              completeUnimported = true,
+              staticcheck = true,
+              directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+              semanticTokens = true,
+            },
+          },
+        },
+      },
+    },
+    setup = {
+      gopls = function(_, opts)
+        -- workaround for gopls not supporting semanticTokensProvider
+        -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
+        LazyVim.lsp.on_attach(function(client, _)
+          if client.name == "gopls" then
+            if not client.server_capabilities.semanticTokensProvider then
+              local semantic = client.config.capabilities.textDocument.semanticTokens
+              client.server_capabilities.semanticTokensProvider = {
+                full = true,
+                legend = {
+                  tokenTypes = semantic.tokenTypes,
+                  tokenModifiers = semantic.tokenModifiers,
+                },
+                range = true,
+              }
+            end
+          end
+        end)
+        -- end workaround
+      end,
+    },
   },
 }
