7d2d4c109a227ae62c598ae53bbb116d92b904fc
David Harrigan <davidharrigan@users.noreply.github.com>
2024-04-03 17:01:33 -0400

    telescope mappings

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -5,6 +5,21 @@ return {
     enabled = "false",
   },
 
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+      local actions = require("telescope.actions")
+      opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
+        mappings = {
+          i = {
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-k>"] = actions.move_selection_previous,
+          },
+        },
+      })
+    end,
+  },
+
   -- notification popup
   {
     "rcarriga/nvim-notify",
