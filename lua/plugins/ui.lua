d57c24e3ba742cec0da6f182b14c8c195f063ff7
David Harrigan <davidharrigan@users.noreply.github.com>
2024-10-22 17:03:49 -0400

    stuff

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,5 +1,25 @@
 return {
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    cmd = "Neotree",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = vim.uv.cwd() })
+        end,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      {
+        "<leader>fE",
+        function()
+          require("neo-tree.command").execute({ toggle = true, dir = LazyVim.root() })
+        end,
+        desc = "Explorer NeoTree (Root Dir)",
+      },
+    },
+  },
+  {
     "folke/noice.nvim",
     opts = {
       ---@type NoiceConfigViews
